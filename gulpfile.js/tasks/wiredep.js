eb1f1e02c6fcb23e4332c639b46617599e381422
Daniel Kita <t.trax0@gmail.com>
2015-10-15 13:42:39 +0200

    Fixed problem with wiredep in scss files

--- gulpfile.js/tasks/wiredep.js
+++ gulpfile.js/tasks/wiredep.js
@@ -3,7 +3,12 @@ var config = require('../config');
 var path = require('path');
 var wiredep = require('wiredep').stream;
 
-var options = {
+gulp.task('wiredep', function() {
+    gulp.run('wiredep-html');
+    gulp.run('wiredep-sass');
+});
+
+var optionsHTML = {
     ignorePath: '../../../' + path.normalize(config.root.dest) + '/',
     overrides: {
         'modernizr': {
@@ -24,9 +29,25 @@ var options = {
     devDependencies: true,
     dependencies: true,
 };
-gulp.task('wiredep', function(callback) {
-    gulp.src([path.resolve(config.root.src, '**/*.{html,scss}'),])
-        .pipe(wiredep(options))
-        .pipe(gulp.dest(path.resolve(config.root.src , config.tasks.html.src)));
+
+gulp.task('wiredep-html', function(callback) {
+    gulp.src([path.resolve(config.root.src, '**/*.html'),])
+        .pipe(wiredep(optionsHTML))
+        .pipe(gulp.dest(path.resolve(config.root.src)));
     callback();
 });
+
+
+var optionsSASS = {
+    devDependencies: true,
+    dependencies: true
+}
+
+
+gulp.task('wiredep-sass', function(callback) {
+    gulp.src([path.resolve(config.root.src, '**/*.scss'),])
+        .pipe(wiredep(optionsSASS))
+        .pipe(gulp.dest(path.resolve(config.root.src)));
+    callback();
+});
+
