c02b78797060f601322e77cfbf3a22f2fd3b735b
Daniel Kita <t.trax0@gmail.com>
2015-10-19 11:27:09 +0200

    Add more extinsions for webfont

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -43,7 +43,7 @@ module.exports = {
             src: 'assets/icons',
             dest: 'assets/fonts',
             sassDest: '',
-            extensions: ['woff2', 'woff']
+            extensions: ['woff2', 'woff', 'eot', 'ttf', 'svg']
         }
     }
 }
