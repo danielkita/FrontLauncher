ef489ba69fce10a8f99b4621f12c0f7d4d4df162
Daniel Kita <t.trax0@gmail.com>
2016-02-03 20:18:05 +0100

    Fixes clean task (resolved problem with permissions

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -60,7 +60,7 @@ module.exports = {
         iconFont: {
             src: 'assets/icons',
             dest: 'assets/fonts',
-            sassDest: '',
+            sassDest: 'partials/',
             extensions: ['woff2', 'woff', 'eot', 'ttf', 'svg']
         }
     }
--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -4,10 +4,7 @@ var path   = require('path')
 var config = require('../config')
 
 var cleanTask = function (cb) {
-  del([config.root.dest]).then(function (paths) {
-    cb()
-  })
-  del([path.resolve(config.root.src,config.tasks.sass.src,'_icons.scss')]).then(function (paths) {
+  del([config.root.dest,path.resolve(config.root.src,config.tasks.sass.src,'partials/_icons.scss')]).then(function (paths) {
     cb()
   })
 }
