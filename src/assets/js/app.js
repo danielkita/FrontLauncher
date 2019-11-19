567a3aeaa6ae4fb53712b74815f4baf173179eec
Daniel Kita <t.trax0@gmail.com>
2019-11-19 20:52:40 +0100

    Add prettier

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,66 +1,60 @@
 /*
-* PLUGINS
-*/
-import slick from "slick-carousel";
-import "slick-carousel/slick/slick.css";
-import Foundation from "foundation-sites";
-import { default as swal } from "sweetalert2";
-require("@fancyapps/fancybox/dist/jquery.fancybox.css");
-const fancybox = require("@fancyapps/fancybox");
-import is from "is_js";
+ * PLUGINS
+ */
+import slick from 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import Foundation from 'foundation-sites';
+import { default as swal } from 'sweetalert2';
+require('@fancyapps/fancybox/dist/jquery.fancybox.css');
+const fancybox = require('@fancyapps/fancybox');
+import is from 'is_js';
 
 /*
-* APP
-*/
+ * APP
+ */
 const App = {
   isTouch() {
     return is.mobile() || is.tablet();
   },
   getGoogleMapsApiKey() {
-    return window.googleMapsApiKey || "";
+    return window.googleMapsApiKey || '';
   },
   initialize() {
-    $(".fancybox").fancybox();
+    $('.fancybox').fancybox();
     $(document).foundation();
   },
   initMap() {
-    const key =
-      this.getGoogleMapsApiKey() !== ""
-        ? `?key=${this.getGoogleMapsApiKey()}`
-        : "";
-    const findMap = document.getElementsByClassName("gmap");
-    const scriptUrl = "http://maps.google.com/maps/api/js" + key;
+    const key = this.getGoogleMapsApiKey() !== '' ? `?key=${this.getGoogleMapsApiKey()}` : '';
+    const findMap = document.getElementsByClassName('gmap');
+    const scriptUrl = 'http://maps.google.com/maps/api/js' + key;
 
     const loadMap = function() {
       for (let i = findMap.length - 1; i >= 0; i--) {
         let myMap = findMap[i];
 
-        let center = new google.maps.LatLng(
-          myMap.getAttribute("data-lat"),
-          myMap.getAttribute("data-lon")
-        );
-        let zoom = parseInt(myMap.getAttribute("data-zoom")) || 13;
+        let center = new google.maps.LatLng(myMap.getAttribute('data-lat'), myMap.getAttribute('data-lon'));
+        let zoom = parseInt(myMap.getAttribute('data-zoom')) || 13;
 
         let map = new google.maps.Map(myMap, {
           zoom,
           center,
           disableDefaultUI: false,
           draggable: !App.isTouch(),
-          scrollwheel: false
+          scrollwheel: false,
         });
 
         let markerOptions = {
           map: map,
           //  icon: 'assets/img/point.png',
-          position: center
+          position: center,
         };
 
         let marker = new google.maps.Marker(markerOptions);
       }
     };
     if (findMap.length) {
-      const s = document.createElement("script");
-      s.setAttribute("src", scriptUrl);
+      const s = document.createElement('script');
+      s.setAttribute('src', scriptUrl);
       s.onload = loadMap;
       document.body.appendChild(s);
     }
@@ -70,7 +64,7 @@ const App = {
   },
   init() {
     document.addEventListener(
-      "DOMContentLoaded",
+      'DOMContentLoaded',
       () => {
         this.initialize();
         this.initMap();
@@ -81,7 +75,7 @@ const App = {
     window.onload = () => {};
 
     window.onresize = () => {};
-  }
+  },
 };
 window.showMessage = App.showMessage;
 App.init();
